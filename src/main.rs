2d14664145c5841feb0d5ab78503db2f9b83b268
Jorge Garcia Rodriguez <jorge@immutable.pro>
2021-12-03 17:15:25 +0000

    day3 part1

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
 use advent_of_code_2021::days::*;
 
 fn main() {
-    d01::part1();
-    d01::part2();
-    d02::part1();
-    d02::part2();
+    // d01::part1();
+    // d01::part2();
+    // d02::part1();
+    // d02::part2();
+    d03::part1();
+    d03::part2();
 }
