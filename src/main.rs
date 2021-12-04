74e42d017948849e74c9d0a55fe4692803789bf3
Jorge Garcia Rodriguez <jorge@immutable.pro>
2021-12-04 22:48:04 +0000

    day4 part1

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,8 @@ fn main() {
     // d01::part2();
     // d02::part1();
     // d02::part2();
-    d03::part1();
-    d03::part2();
+    // d03::part1();
+    // d03::part2();
+    d04::part1();
+    d04::part2();
 }
